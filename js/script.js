2ac81affbcb0dad1ea6336d80bcd4cdd084d927c
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-01-03 21:44:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -94,3 +94,13 @@ window.addEventListener("load", function () {
    loader.style.display = "none";
 });
 // <=>
+
+// 100vh form mobile <=====================================>
+const documentHeight = () => {
+   const doc = document.documentElement
+   doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', documentHeight)
+documentHeight()
+
+// <=>
